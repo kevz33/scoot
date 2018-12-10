e879b0ad832ffff1e3d10a68afe373ca363b63d0
Kevin Castillo <kcastillo@csumb.edu>
2018-12-10 21:39:56 +0000

    home and homestyle

--- home.php
+++ home.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+    
+
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -11,15 +16,18 @@
         <div id="home">
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-            <form action="">
+            <form id="search" action="">
               <input type="search">
               <i class="fa fa-search"></i>
             </form>
         <nav>
           <a style="margin-right:200px"href="profile.html"> My Profile </a>
         
-          <a href="index.html"> Logout </a>
+          <a href="logout.php"> Logout </a>
        </nav>
-        </h1>
+      
+
+       
   </body>
 </html>
+
---
+++ logout.php
@@ -0,0 +1,8 @@
+<?php 
+//include 'functions.php';
+//checkedLoggedIn();
+session_start();
+session_destroy();
+
+//header("location: index.php")
+?>
