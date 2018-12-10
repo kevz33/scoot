19deb94315f6b5c8c5cda4806b7d78ef751e05b5
Aaron Parks <aaparks@csumb.edu>
2018-12-10 21:28:42 +0000

    updated database to work for everyone

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Home Page</title>
     <link rel="stylesheet" type="text/css" href="styles/homePageStyle.css">
-    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+    
   </head>
   <body>
         <h1><img src="images/scoot.png" id="logo">
