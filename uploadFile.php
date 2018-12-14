fc34ee8f3f93474684efcd6e80b60a4ea6b7d021
Kevin Castillo <kcastillo@csumb.edu>
2018-12-14 10:23:32 +0000

    finally

--- uploadFile.php
+++ uploadFile.php
@@ -89,6 +89,7 @@ if(isset($_POST['uploadText'])){
     <meta charset="UTF-8">
     <title>Scoot</title>
     <link rel="stylesheet" type="text/css" href="styles/uploadStyles.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
   <body>
